6588e835df3e08d29ce1738a454e9d0f2d4ba345
Nox <bgatti165@gmail.com>
2026-06-06 23:13:18 +0000

    KAN-3 feat: analisis de ventas con pandas

--- scripts/analisis_ventas.py
+++ scripts/analisis_ventas.py
@@ -3,4 +3,25 @@ import pandas as pd
 
 df = pd.read_csv("datos/ventas.csv")
 
-print(df.head())
+df["Venta_Total"] = df["Cantidad"] * df["Precio"]
+
+total_vendido = df["Venta_Total"].sum()
+promedio_ventas = df["Venta_Total"].mean()
+
+producto_mas_vendido = (
+    df.groupby("Producto")["Cantidad"]
+    .sum()
+    .idxmax()
+)
+
+ventas_categoria = (
+    df.groupby("Categoria")["Venta_Total"]
+    .sum()
+)
+
+print("TOTAL VENDIDO:", total_vendido)
+print("PROMEDIO:", promedio_ventas)
+print("PRODUCTO MÁS VENDIDO:", producto_mas_vendido)
+
+print("\nVENTAS POR CATEGORIA")
+print(ventas_categoria)
